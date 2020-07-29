5288ee2b4e6455cbebe7ea4ae31efa974e829f26
roffer <497664492@qq.com>
2020-07-29 17:26:14 +0800

    init.

---
+++ src/components/schedule.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="schedule">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "schedule",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style scoped lang="less">
+
+</style>
