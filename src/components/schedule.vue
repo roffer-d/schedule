70e898fbd9a9a88aab691d7a85c7d2ef90921116
roffer <497664492@qq.com>
2020-08-05 09:27:39 +0800

    版本更新

--- src/components/schedule.vue
+++ src/components/schedule.vue
@@ -144,8 +144,8 @@
                 currentDateStr: '',
                 swiperList: [],
                 swiperOptions: {
-                    observer: true,//修改swiper自己或子元素时，自动初始化swiper
-                    observeParents: true//修改swiper的父元素时，自动初始化swiper
+                    // observer: true,//修改swiper自己或子元素时，自动初始化swiper
+                    // observeParents: true//修改swiper的父元素时，自动初始化swiper
                 }
             }
         },
